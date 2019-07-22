ac33cdc5f0e261863061827e4547470b5d70190f
Jesus Ruiz Rodriguez <jaruiz@tiempodevelopment.com>
2019-07-22 14:39:28 -0700

    Add project files.

---
+++ Algorithms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+	class Program
+	{
+		static int M = 26;
+		static void Main(string[] args)
+		{
+			Console.WriteLine(isValid("aabbcd"));
+		}
+
+		static string isValid(string s)
+		{
+
+			int l = s.Length;
+			int[] freq = new int[M];
+			for (int i = 0; i < s.Length; i++)
+			{
+				freq[s[i] - 'a']++;
+			}
+
+			return "YES";
+		}
+	}
+}
